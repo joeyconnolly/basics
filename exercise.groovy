512375421372e25af16fe08b70f6f9aea0335de5
Joey Connolly <Joey Connolly>
2016-10-30 16:18:09 +0000

    First commit

---
+++ exercise.groovy
@@ -0,0 +1,15 @@
+int tot = 0
+
+print "Enter your first number: "
+String str = System.console().readLine()
+int num = Integer.parseInt(str)
+
+print "Enter your first number: "
+String str2 = System.console().readLine()
+int num2 = Integer.parseInt(str2)
+
+for (i=0;i<num2;i++) {
+tot = tot + num
+}
+
+print tot
